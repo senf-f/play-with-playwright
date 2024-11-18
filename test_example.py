de45f893d6be4c7e9deab2db7bb7ececd775a188
mate.mrse <mate.mrse@gmail.com>
2024-11-18 13:08:14 +0100

    Added a positive test

--- test_example.py
+++ test_example.py
@@ -2,8 +2,15 @@ import re
 from playwright.sync_api import Page, expect
 
 
-def test_has_title(page: Page):
+def test_has_wrong_title(page: Page):
     page.goto("https://playwright.dev/")
 
     # Expect a title "to contain" a substring.
     expect(page).to_have_title(re.compile("Playwrightz"))
+
+
+def test_has_title(page: Page):
+    page.goto("https://playwright.dev/")
+
+    # Expect a title "to contain" a substring.
+    expect(page).to_have_title(re.compile("Playwright"))
